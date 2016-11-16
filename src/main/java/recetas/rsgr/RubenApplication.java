4e7e5ab6b5aff2f76abbf01efa0150f4906582aa
zukua <zukuaru@hotmail.com>
2016-11-15 19:50:13 -0600

    hola

--- src/main/java/recetas/rsgr/RubenApplication.java
+++ src/main/java/recetas/rsgr/RubenApplication.java
@@ -3,16 +3,19 @@ package recetas.rsgr;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 
 @SpringBootApplication
@@ -23,17 +26,20 @@ public class RubenApplication {
 
 
         try{
-            // Use a file
-
-           // NPOIFSFileSystem fs = new NPOIFSFileSystem(new File("C:\\Users\\zukua\\Documents\\uno.xlsx"));
-
-
-            Workbook wb = WorkbookFactory.create(new FileInputStream("C:\\Users\\zukua\\Documents\\uno.xlsx"));
+            FileInputStream fileIn = null;
+            FileOutputStream fileOut = null;
 
 
-         Sheet s1= wb.getSheetAt(0);
-         Row r1= s1.getRow(0);
-   System.out.println("Bien");
+
+
+              //  fileIn = new FileInputStream("C:\\Users\\zukua\\Documents\\uno.xlsx");
+            OPCPackage pkg = OPCPackage.open("C:\\Users\\zukua\\Documents\\uno.xlsx");
+            XSSFWorkbook wb = new XSSFWorkbook("C:\\Users\\zukua\\Documents\\uno.xlsx");
+           // XSSFWorkbook wb = new XSSFWorkbook(fileIn);
+            XSSFSheet sheet = wb.getSheetAt(0);
+            XSSFRow row = sheet.getRow(2);
+
+                System.out.println("Bien");
 
         }catch(Exception e){
 System.out.println("mal "+e.getMessage());
