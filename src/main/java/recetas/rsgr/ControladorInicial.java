79d454621c1ffc1ec9385e96f6ddac9f2257ace2
como-quesito <rapidclimate@outlook.com>
2016-11-22 19:22:26 -0600

    Ya esta bien

--- src/main/java/recetas/rsgr/ControladorInicial.java
+++ src/main/java/recetas/rsgr/ControladorInicial.java
@@ -1,7 +1,11 @@
 package recetas.rsgr;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.joda.time.DateTime;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by zukua on 28/10/2016.
@@ -12,6 +16,43 @@ public class ControladorInicial {
     @RequestMapping("/hola")
     public String hola(){
         return "segunda pureba";
+    }
+
+
+
+    @CrossOrigin
+    @RequestMapping(value="/cargar-mongo1", method= RequestMethod.POST, headers={"Accept=text/html"})
+    public @ResponseBody
+    String handleFileUpload(@RequestParam("file") MultipartFile file)throws Exception{
+        String nombre=file.getOriginalFilename();
+        String prefijo="";
+        DateTime date=new DateTime();
+        ;
+        int dia=  date.getDayOfYear();
+        int segundo=  date.getSecondOfDay();
+        long tamano= file.getSize();
+        String fileName = null;
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        if (file.getSize() > 0) {
+            inputStream = file.getInputStream();
+
+
+            String contenido=  file.getContentType();
+            int punto=nombre.indexOf(".");
+            String nombreSolo=nombre.substring(0, punto);
+            prefijo="dia"+dia+"segundo"+segundo;
+            //   System.out.println("nombre de archivo:"+fileName);
+            //Guardamos imagen, si es que hay
+            // gridFsTemplate.store(inputStream,prefijo+nombre,file.getContentType());
+            //Lo impresionante aqui es que el repositorio tambien tiene la opcion para
+            //guardar un archivo!!!
+            // grid.store(inputStream,prefijo+nombre,file.getContentType());
+        }
+        System.out.println("El nombre de archivaldo es:" + nombre + " el tamaño del archivo esta:" + tamano + " se guardo como: " + prefijo + nombre);
+
+        return  prefijo+nombre;
+
     }
 
 }
--- src/main/java/recetas/rsgr/MiUi.java
+++ src/main/java/recetas/rsgr/MiUi.java
@@ -1,13 +1,15 @@
 package recetas.rsgr;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 /**
  * Created by zukua on 04/11/2016.
@@ -19,12 +21,20 @@ public class MiUi extends UI{
     protected void init(VaadinRequest request) {
         VerticalLayout layout = new VerticalLayout();
 
+
+
         Label label = new Label("NEGADO");
         Button boton=new Button("UMF mas negadas");
         boton.addStyleName(ValoTheme.BUTTON_PRIMARY);
 
         layout.addComponent(label);
         layout.addComponent(boton);
+
+
+
+
         setContent(layout);
     }
 }
+
+
